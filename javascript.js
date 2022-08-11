6f4186da7ce6a21b4c3c0848a1781a74bc64f9d6
aecel <aecelacuna@gmail.com>
2022-08-11 14:42:01 +0800

    Add functions (add movie, remove movie, toggle watched)

--- javascript.js
+++ javascript.js
@@ -64,14 +64,29 @@ const newLibrary = () => {
   return { addMovie, listMovies, removeMovie, getMovie }
 }
 
-const newMovie = (title, genre, rating, watched) => {
+const newMovie = (title, year, genre, rating, watched) => {
   const getTitle = () => title
+  const getYear = () => {
+    if(year) {
+      return `(${year})`
+    } else {
+      return ""
+    }
+  }
+  
   const getGenre = () => capitalizeFirstLetter(genre)
   const getWatched = () => {
     if (watched) {
-      return "✔ Watched"
+      return "Watched"
     } else {
-      return "✘ Not Watched"
+      return "Not Watched"
+    }
+  }
+  const getWatchedThings = () => {
+    if (watched) {
+      return ["#5fc195", "./images/checkmark.svg"]
+    } else {
+      return ["#c15f5f", "./images/cross.svg"]
     }
   }
 
@@ -101,8 +116,10 @@ const newMovie = (title, genre, rating, watched) => {
 
   return {
     getTitle,
+    getYear,
     getGenre,
     getWatched,
+    getWatchedThings,
     getGenreImage,
     getRatingPattern,
     displayMovie,
@@ -111,17 +128,19 @@ const newMovie = (title, genre, rating, watched) => {
 }
 
 const myMovies = newLibrary()
-const morbius = newMovie("Morbius", "thriller", 4, false)
-const tropicThunder = newMovie("Tropic Thunder", "comedy", 5, true)
-const shrek = newMovie("Shrek", "animation", 1, true)
+const morbius = newMovie("Morbius", 2022, "action", 5, false)
+const tropicThunder = newMovie("Tropic Thunder", 2008, "comedy", 5, true)
+const shrek = newMovie("Shrek", 2001, "animation", 5, true)
 myMovies.addMovie(morbius)
 myMovies.addMovie(tropicThunder)
 myMovies.addMovie(morbius)
 myMovies.addMovie(shrek)
 myMovies.addMovie(
-  newMovie("Charlie and the Chocolate Factory", "horror", 3, false)
+  newMovie("Charlie and the Chocolate Factory", 2005, "horror", 3, false)
 )
-myMovies.addMovie(newMovie("Bridesmaids", "comedy", 5, false))
+myMovies.addMovie(newMovie("Bridesmaids", 2011, "comedy", 5, false))
+myMovies.addMovie(newMovie("The Notebook", 2004, "romance", 3, true))
+myMovies.addMovie(newMovie("Citizen Kane", 1941, "classic", 2, true))
 console.log(myMovies.listMovies())
 
 const movieToToggle = myMovies.getMovie("Morbixus")
@@ -129,25 +148,28 @@ if (movieToToggle) {
   movieToToggle.toggleWatched()
 }
 
-myMovies.removeMovie("Tropic Thunder")
-myMovies.removeMovie("Chicken Little")
-myMovies.removeMovie("Tropic Thunder")
-
 console.log(myMovies.listMovies())
 
 // TEMP make movie cards for each movie in the library
 const appendMovieCard = (movie) => {
   const html = `
-    <div class="movie-card">
+    <div class="movie-card" data-title="${movie.getTitle()}">
       <div class="movie-card-text">
-        <p style="font-weight: bold">${movie.getTitle()} (Year)</p>
+        <p style="font-weight: bold">${movie.getTitle()}  ${movie.getYear()}</p>
         <p>Genre: ${movie.getGenre()}</p>
         <p class="rating">
           Rating:  
           ${movie.getRatingPattern()}
         </p>
         <p>
-          <div class="watched">${movie.getWatched()}</div>
+          <div class="watched"> 
+          <div class="watched-icon-container" style="background-color: ${
+            movie.getWatchedThings()[0]
+          };" >
+            <img src=${movie.getWatchedThings()[1]} class="watched-icon">
+          </div>
+          <div class="watched-text">${movie.getWatched()}</div>
+          </div>
         </p>
       </div>
       <div class="movie-card-img">
@@ -159,9 +181,7 @@ const appendMovieCard = (movie) => {
       <div class="remove-movie">
         <img src="./images/trash.svg" class="remove-movie-img">
       </div>
-      <div class="edit-movie">
-        <img src="./images/edit.svg" class="edit-movie-img">
-      </div>
+      
     </div>
   `
 
@@ -169,9 +189,96 @@ const appendMovieCard = (movie) => {
   movieCardContainer.innerHTML += html
 }
 
-for (const movie of myMovies.listMovies()) {
-  appendMovieCard(movie)
+const refreshMovieDisplay = () => {
+  const movieCardContainer = document.querySelector(".movie-cards")
+  movieCardContainer.innerHTML = ""
+
+  for (const movie of myMovies.listMovies()) {
+    appendMovieCard(movie)
+  }
 }
+
+const connectWatchedFunction = () => {
+  const watchedIcons = document.getElementsByClassName("watched-icon-container")
+  for (const icon of watchedIcons) {
+    icon.addEventListener("click", whatHappensWhenYouClickWatched)
+  }
+}
+
+const whatHappensWhenYouClickWatched = (event) => {
+  const container = event.target.closest(".movie-card")
+  const movie = myMovies.getMovie(container.dataset.title)
+  movie.toggleWatched()
+  updatePage()
+}
+
+const connectRemoveFunction = () => {
+  const removeIcons = document.getElementsByClassName("remove-movie")
+  for (const icon of removeIcons) {
+    icon.addEventListener("click", whatHappensWhenYouClickRemove)
+  }
+}
+
+const whatHappensWhenYouClickRemove = (event) => {
+  const container = event.target.closest(".movie-card")
+  myMovies.removeMovie(container.dataset.title)
+  updatePage()
+}
+
+const updatePage = () => {
+  refreshMovieDisplay()
+  connectWatchedFunction()
+  connectRemoveFunction()
+}
+
+updatePage()
+
+// Get the modal
+const modal = document.querySelector(".modal")
+
+// Get the button that opens the modal
+const addMovieButton = document.getElementById("add-movie-btn")
+
+// Get the <span> element that closes the modal
+const closeButton = document.getElementsByClassName("close")[0]
+
+// When the user clicks the button, open the modal
+addMovieButton.addEventListener("click", () => {
+  modal.style.display = "block"
+})
+
+// When the user clicks on <closeButton> (x), close the modal
+closeButton.addEventListener("click", () => {
+  closeForm()
+})
+
+const submitForm = (event) => {
+  event.preventDefault()
+  const form = event.target
+  const formData = new FormData(form)
+
+  const title = formData.get("movie-title")
+  const year = formData.get("movie-year")
+  const genre = formData.get("movie-genre")
+  const rating = formData.get("movie-rating")
+  let watched = false
+  if (formData.get("movie-watched")) {
+    watched = true
+  }
+  const movie = newMovie(title, year, genre, rating, watched)
+
+  myMovies.addMovie(movie)
+  updatePage()
+  form.reset()
+  closeForm()
+}
+
+const closeForm = () => {
+  modal.style.display = "none"
+}
+
+const movieForm = document.getElementById("add-movie-form")
+movieForm.addEventListener("submit", submitForm)
 
 // Object constructor example
 // function Book(author, title, pages, read) {
